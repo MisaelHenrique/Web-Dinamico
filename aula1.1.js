70142d1bfbea8c0ae24771a9d9eaacd1853062a8
MisaelHenrique <misaelhenriquesilva@gmail.com>
2023-07-25 22:40:23 -0400

    aula e exercicio 1

---
+++ aula1.1.js
@@ -0,0 +1,19 @@
+//1 criar um programa para ler uma variavel e imprimir seu valor
+//2 criar um programa para ler uma variavel e dizer se é par ou impar
+//3 criar um programa para ler 3 variaveis e testar com operadores relacionais
+
+//1
+let a = 'Misael'
+console.log(a)
+
+//2
+let number = 10;
+
+if (number % 2 === 0) {  
+    console.log('par')
+}else{
+    console.log('impar')
+}
+
+
+
---
+++ aula1.js
@@ -0,0 +1,38 @@
+//declaração de variaveis
+var a = 10 // inteiro
+var b = '20'
+var c = false
+
+//a = 'beto' //troca de tipagem -> objeto string 
+
+let x = 20
+
+const y = 'victor' // isso é uma constante
+
+//----------------------------------------------------------------
+
+var d //quando não inicializada será do tipo undefined
+var e = '' // none | null 
+
+console.log (d)
+
+var arr = new Array ();
+
+var arr2 = new Array(1,2,3,4);
+
+var arr3 = [1,2,3,4,5];
+
+var arr4 = new Array(5);
+
+console.log (arr3);
+
+if(a >= 10){
+    console.log('a maior que 10')
+    let a = 30;
+}else{
+    if(a <= 20){
+        console.log('a menor que 20')
+    }
+}
+
+console.log(a)
