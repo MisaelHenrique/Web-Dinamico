0647ef849dfb8ef4b6cb48df784d1c7e83528ce6
MisaelHenrique <misaelhenriquesilva@gmail.com>
2023-08-13 18:15:09 -0400

    lista 1 html

---
+++ lista1/lista1.js
@@ -0,0 +1,62 @@
+//1
+function media(){
+    let n1 = Number(document.querySelector(".n1").value);
+    let n2 = Number(document.querySelector(".n2").value);
+    let n3 = Number(document.querySelector(".n3").value);
+    let media = Number((n1 + n2 + n3) / 3);
+    let result = document.querySelector(".msg")
+    result.innerText = media.toFixed(2);
+    console.log(media)
+    
+}
+
+//2
+function salario(){
+
+}
+
+//3
+function calcular(){
+
+}
+
+//4
+function parcelar(){
+
+}
+
+//5
+function mediaAluno(){
+
+}
+
+//6
+function calcularCodigo(){
+
+}
+
+
+//7
+function grau(){
+
+}
+
+
+//8
+function positivo(){
+
+}
+
+
+//9
+function negativo(){
+
+}
+
+//10
+function idade(){
+
+}
+
+
+
