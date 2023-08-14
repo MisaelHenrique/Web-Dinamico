b8f109ee6d3022f23e933b4b672963b518eea8e4
MisaelHenrique <misaelhenriquesilva@gmail.com>
2023-08-13 21:54:20 -0400

    ex 4 e 5

--- lista1/lista1.js
+++ lista1/lista1.js
@@ -3,7 +3,7 @@ function media(){
     let n1 = Number(document.querySelector(".n1").value);
     let n2 = Number(document.querySelector(".n2").value);
     let n3 = Number(document.querySelector(".n3").value);
-    let media = Number((n1 + n2 + n3) / 3);
+    let media = (n1 + n2 + n3) / 3;
     let result = document.querySelector(".msg")
     result.innerText = media.toFixed(2);
     //console.log(media)
@@ -14,7 +14,7 @@ function media(){
 function salario(){
 
     let nome = document.querySelector(".name").value;
-    let salario = Number (document.querySelector(".salario").value);
+    let salario = Number(document.querySelector(".salario").value);
 
     let acrescimo = Number(salario * 0.10 + salario);
     let aumento = document.querySelector(".aumento")
@@ -34,11 +34,10 @@ function calcular(){
     let numero1 = Number(document.querySelector("#n1").value);
     let numero2 = Number(document.querySelector("#n2").value);
     let numero3
-    console.log(numero1, numero2)
+
     if (numero1 === numero2) {
 
         numero3 = numero1 + numero2;
-        console.log(numero3)
         
     }else{
 
@@ -55,17 +54,69 @@ function calcular(){
 //4
 function parcelar(){
 
-    
+    let preco = Number(document.querySelector(".preco").value);
+    let parcela
+    let total
+    if (document.querySelector("#par3").checked){
 
+        total = preco * 0.1 + preco;
+        parcela = total / 3;
+    }else if (document.querySelector("#par5").checked){
+
+        total =  preco * 0.2 + preco
+        parcela = total / 5
+    }
+
+        let par = document.querySelector("#parcela");
+        let totalParcela = document.querySelector("#total");
+
+        par.innerText = parcela.toFixed(2)
+        totalParcela.innerText = total.toFixed(2)
+
+        
 }
 
 //5
 function mediaAluno(){
 
+    let aluno = document.querySelector("#aluno").value;
+    let nota1 = Number(document.querySelector("#nota1").value);
+    let nota2 = Number(document.querySelector("#nota2").value);
+    let nota3 = Number((document.querySelector("#nota3").value));
+    let situacao = document.querySelector("#situation")
+    
+    let media = (nota1 + nota2 + nota3)/3
+
+    if(media >= 8){
+
+        let total = document.querySelector("#media");
+        total.innerText = media.toFixed(2)
+
+        situacao.innerText = "APROVADO"
+        situacao.style.color = "green" 
+
+    }else{
+
+        let total = document.querySelector("#media");
+        total.innerText = media.toFixed(2)
+
+        situacao.innerText = "REPROVADO"
+        situacao.style.color = "red" 
+    }
+
+    let nomeAluno = document.querySelector("#nome")
+        nomeAluno.innerText = aluno;
+
+
+
 }
 
 //6
 function calcularCodigo(){
+
+    let option = document.querySelector("#codigo");
+
+    let opt  = Number(option.value);
 
 }
 
