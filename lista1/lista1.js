93426de07a476eae09fe67b3e5a9457dc4cb0ee5
MisaelHenrique <misaelhenriquesilva@gmail.com>
2023-08-13 20:17:00 -0400

    ex 2 e 3

--- lista1/lista1.js
+++ lista1/lista1.js
@@ -6,22 +6,56 @@ function media(){
     let media = Number((n1 + n2 + n3) / 3);
     let result = document.querySelector(".msg")
     result.innerText = media.toFixed(2);
-    console.log(media)
+    //console.log(media)
     
 }
 
 //2
 function salario(){
 
+    let nome = document.querySelector(".name").value;
+    let salario = Number (document.querySelector(".salario").value);
+
+    let acrescimo = Number(salario * 0.10 + salario);
+    let aumento = document.querySelector(".aumento")
+    aumento.innerText = acrescimo.toFixed(2)
+
+    let colab = document.querySelector(".funcionario")
+    colab.innerText = nome;
+    // console.log(acrescimo)
+    // console.log(nome)
+
+
 }
 
 //3
 function calcular(){
 
+    let numero1 = Number(document.querySelector("#n1").value);
+    let numero2 = Number(document.querySelector("#n2").value);
+    let numero3
+    console.log(numero1, numero2)
+    if (numero1 === numero2) {
+
+        numero3 = numero1 + numero2;
+        console.log(numero3)
+        
+    }else{
+
+        numero3 = numero1 * numero2;
+    
+    }
+
+    let result = document.querySelector(".result")
+    result.innerText = numero3
+    console.log(numero3)
+
 }
 
 //4
 function parcelar(){
+
+    
 
 }
 
