5930f129aa6ef4d71b3e88ac9bc5bad3433a2078
MisaelHenrique <misaelhenriquesilva@gmail.com>
2023-08-15 23:06:06 -0400

    projeto 01

---
+++ aula04/index.js
@@ -0,0 +1,8 @@
+function alteraTexto(){
+
+    let outroTexto = document.querySelector('#container');
+
+    outroTexto.innerHTML = '<p> Texto Alterado com sucesso! </p>';
+    outroTexto.style.color = 'red';
+
+}
